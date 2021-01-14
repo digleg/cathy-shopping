b9c0526166e294a24ae9b4688394f6675137f3bb
vito huang <digleg_h@hotmail.com>
2021-01-15 00:58:16 +0800

    [store] delete redundant reducer import

--- src/store.js
+++ src/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import RowWrapperSlice from './components/RowWrapper/RowWrapperSlice';
-import counterReducer from './features/counter/counterSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     rowWrapper: RowWrapperSlice,
   },
 });
