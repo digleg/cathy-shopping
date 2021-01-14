787b7a58e20293f8669d0938d2cdd15cdef517aa
vito huang <digleg_h@hotmail.com>
2021-01-15 00:59:27 +0800

    [App] add circular progress-bar and masked effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { isEmpty } from 'lodash';
 import './App.css';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import RowWrapper from './components/RowWrapper';
-import orderData from './orderData';
-import { setProcessingOrder, setCompleteOrder } from './components/RowWrapper/RowWrapperSlice';
+import {
+  setProcessingOrder,
+  setCompleteOrder,
+  selectGetProcessingOrder,
+  selectGetCompleteOrder,
+} from './components/RowWrapper/RowWrapperSlice';
+import db from './firebase';
 
 function App() {
   const dispatch = useDispatch();
@@ -17,29 +24,43 @@ function App() {
     PROCESSING: true,
     COMPLETE: false,
   };
+  const processingOrder = useSelector(selectGetProcessingOrder);
+  const completeOrder = useSelector(selectGetCompleteOrder);
+
+  const isDataEmpty = isEmpty(processingOrder) || isEmpty(completeOrder);
 
   useEffect(() => {
-    dispatch(
-      setProcessingOrder(
-        orderData.orders.filter(
-          (el) =>
-            el.status?.code === ORDER_STATUS.PROCESSING ||
-            el.status?.code === ORDER_STATUS.ESTABILISH,
+    // 至firebase db撈取order資料
+    db.collection('orders').onSnapshot((snapshot) => {
+      // 進行中項目
+      dispatch(
+        setProcessingOrder(
+          snapshot.docs
+            .map((doc) => doc.data())
+            .filter(
+              (el) =>
+                el.status?.code === ORDER_STATUS.PROCESSING ||
+                el.status?.code === ORDER_STATUS.ESTABILISH,
+            ),
         ),
-      ),
-    );
-    dispatch(
-      setCompleteOrder(
-        orderData.orders.filter(
-          (el) =>
-            el.status?.code === ORDER_STATUS.CANCEL || el.status?.code === ORDER_STATUS.ARRIVAL,
+      );
+      // 已完成項目
+      dispatch(
+        setCompleteOrder(
+          snapshot.docs
+            .map((doc) => doc.data())
+            .filter(
+              (el) =>
+                el.status?.code === ORDER_STATUS.CANCEL || el.status?.code === ORDER_STATUS.ARRIVAL,
+            ),
         ),
-      ),
-    );
+      );
+    });
   }, []);
 
   return (
-    <div className="app">
+    <div className={`app ${isDataEmpty && 'app__mask'}`}>
+      {isDataEmpty && <CircularProgress size={80} />}
       <RowWrapper status={ROWWRAPPER_STATUS.PROCESSING} />
       <RowWrapper status={ROWWRAPPER_STATUS.COMPLETE} />
     </div>
