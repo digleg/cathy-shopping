19ed7757845987355f7a6bde89358922f761fbdc
vito huang <digleg_h@hotmail.com>
2021-01-14 15:39:55 +0800

    cathy-shopping project initial

---
+++ src/components/Row/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Row() {
+  return (
+    <div>
+      <h1>I'm Row</h1>
+    </div>
+  );
+}
+
+export default Row;
---
+++ src/components/RowWrapper/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Row from '../Row';
+
+function RowWrapper() {
+  return (
+    <div>
+      進行中
+      <Row />
+      <Row />
+      <Row />
+    </div>
+  );
+}
+
+export default RowWrapper;
